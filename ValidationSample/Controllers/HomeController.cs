acf9bfdc133ee2b1365dc9356b722d4be9ae5337
Darren Neimke <darren.neimke@gmail.com>
2019-05-07 18:59:54 +0800

    Add project files.

---
+++ ValidationSample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using ValidationSample.Features.Home;
+using ValidationSample.Models;
+
+namespace ValidationSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public HomeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index(WelcomeMessage.Request request)
+        {
+            var result = await _mediator.Send(request);
+            return View(result);
+        }
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ValidationSample/Features/Home/WelcomeMessage.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ValidationSample.Features.Home
+{
+    public static class WelcomeMessage
+    {
+        public class Request : IRequest<ViewModel>
+        {
+            public string Name { get; set; }
+        }
+
+        public class ViewModel
+        {
+            public string Message { get; set; }
+        }
+
+        public class RequestHandler : IRequestHandler<Request, ViewModel>
+        {
+            public Task<ViewModel> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var message = string.IsNullOrEmpty(request.Name) ? "Who are you?" : $"Hello {request.Name}";
+                return Task.FromResult(new ViewModel { Message = message });
+            }
+        }
+    }
+}
